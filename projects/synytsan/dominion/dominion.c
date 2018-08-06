6a7f27822d72ba0705100ae06280deece7cb4521
AntonSynytsia <anton.synytsia@gmail.com>
2018-08-05 18:35:23 -0700

    Updated dominion bug to the one in assignment 3

--- projects/synytsan/dominion/dominion.c
+++ projects/synytsan/dominion/dominion.c
@@ -661,7 +661,7 @@ int adventurerEffect(int currentPlayer, struct gameState *state) {
   int temphand[MAX_HAND];
   int drawntreasure = 0;
   int cardDrawn;
-  int z = 0; // this is the counter for the temp hand
+  int z = 1; // this is the counter for the temp hand
 
   while(drawntreasure<2){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
@@ -674,12 +674,12 @@ int adventurerEffect(int currentPlayer, struct gameState *state) {
     else{
       temphand[z]=cardDrawn;
       state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-      z++;
+      ++z;
     }
   }
   while(z-1>=0){
     state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-    ++z;
+    --z;
   }
   return 0;
 }
--- projects/synytsan/dominion/dominion.h
+++ projects/synytsan/dominion/dominion.h
@@ -128,4 +128,10 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int smithyEffect(int currentPlayer, struct gameState *state, int handPos);
+int adventurerEffect(int currentPlayer, struct gameState *state);
+int councilRoomEffect(int currentPlayer, struct gameState *state, int handPos);
+int salvagerEffect(int currentPlayer, struct gameState *state, int handPos, int choice1);
+int baronEffect(int currentPlayer, struct gameState *state, int handPos, int choice1);
+
 #endif
